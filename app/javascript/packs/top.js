617152a65b6f822dd4918db9aef7fb1a3a2b65bf
wadayuki <106656539+lu3-yk@users.noreply.github.com>
2022-09-18 10:33:30 +0000

    todo完成

--- app/javascript/packs/top.js
+++ app/javascript/packs/top.js
@@ -1,24 +1,84 @@
 $(document).ready(function(){
     console.log("loaded")
 
+    const form = document.getElementById("form");
+    const input = document.getElementById("input");
+    const ul = document.getElementById("ul");
 
-     $("#form").submit(function(event){
-        //event.preventDefault();
-        const input = $("#input").val();
-         console.log(input);
-         return false;
-         add();
-     })
-    function add()  {
-        const li = document.createElement("li");
-        li.innerTxt = input.value;
-        li.classList.add("list-group-item");
-        ul.appendChild(li);
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos) {
+      todos.forEach((todo) => {
+        add(todo);
+      });
     }
+
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      add();
+    });
+
+    function add(todo) {
+      let todoText = input.value;
+
+      if (todo) {
+        todoText = todo.text;
+      }
+
+      if (todoText) {
+        const li = document.createElement("li");
+
+        li.innerText = todoText;
+        li.classList.add('list-group-item')
+
+        if (todo && todo.completed) {
+          li.classList.add("text-decoration-line-through");
+        }
+
+        li.addEventListener("contextmenu", function (event) {
+          event.preventDefault();
+          li.remove();
+          saveData();
+        });
+
+        li.addEventListener("click", function () {
+          li.classList.toggle("text-decoration-line-through");
+          saveData();
+        });
+
+        ul.appendChild(li);
+        input.value = "";
+        saveData();
+      }
+    }
+
+    function saveData() {
+      const lists = document.querySelectorAll("li");
+      const todos = [];
+
+      lists.forEach((li) => {
+        todos.push({
+          text: li.innerText,
+          completed: li.classList.contains("text-decoration-line-through"),
+        });
+      });
+
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
+    
 })
 
-// form.addEventListener("submit",function(event){
-//     event.preventDefault();
-//     console.log(input.value);
-// });
 
+
+
+    //  $("#form").submit(function(event){
+    //     //event.preventDefault();
+    //     const input = $("#input").val();
+    //      console.log(input);
+    //      return false;
+    //      add();
+    //  })
+    // function add()  {
+    //     const li = document.createElement("li");
+    //     li.innerTxt = input.value;
+    //     li.classList.add("list-group-item");
+    //     ul.appendChild(li);
